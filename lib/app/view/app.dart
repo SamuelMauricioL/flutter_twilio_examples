9ccf240606248422e912f7f6a8bb4260df1e9899
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-02-20 20:42:08 -0500

    chore: integrated sms sending

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_twilio_examples/counter/counter.dart';
 import 'package:flutter_twilio_examples/l10n/l10n.dart';
+import 'package:twilio_flutter/twilio_flutter.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -27,7 +28,62 @@ class App extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const TwilioPage(),
+    );
+  }
+}
+
+class TwilioPage extends StatefulWidget {
+  const TwilioPage({Key? key}) : super(key: key);
+
+  @override
+  State<TwilioPage> createState() => _TwilioPageState();
+}
+
+class _TwilioPageState extends State<TwilioPage> {
+  late TwilioFlutter twilioSendSms;
+  late TwilioFlutter twilioSendWsp;
+
+  @override
+  void initState() {
+    twilioSendSms = TwilioFlutter(
+      accountSid: '-AC7343bf1a667dd7be216a349cc50c7e93',
+      authToken: '-e211af0e9f718e43cc3ba4d66948a6f6',
+      twilioNumber: '+19036368506',
+    );
+
+    twilioSendWsp = TwilioFlutter(
+      accountSid: '-AC7343bf1a667dd7be216a349cc50c7e93',
+      authToken: '-e211af0e9f718e43cc3ba4d66948a6f6',
+      twilioNumber: '+14155238886',
+    );
+
+    super.initState();
+  }
+
+  Future<void> sendSms() async {
+    await twilioSendSms.sendSMS(
+      toNumber: '+51923873749',
+      messageBody: 'Hola, buen día',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Usando Twilio'),
+      ),
+      body: const Center(
+        child: Text(
+          'Click the button to send SMS.',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: sendSms,
+        tooltip: 'Send Sms',
+        child: const Icon(Icons.send),
+      ),
     );
   }
 }
